96530fd42201ae3dc4098f14e757605ca2b2a45d
MedRiadh2001 <mohamedriadh.essridi@polytechnicien.tn>
2026-01-08 02:32:49 +0100

    update

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,7 @@ app.get('/', (req, res)=>{
 app.use('/uploads', express.static('uploads'));
 require('./src/api/routes/routes')(app)
 
-app.listen(process.env.PORT, ()=>{
-    console.log ('listening on port', process.env.PORT);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Serveur prêt sur le port ${PORT}`);
 });
--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 const config={
-    DB_URL:process.env.DATABASE_URL
+    DB_URL:process.env.DATABASE_URL || "mongodb://mongodb-service:27017/exam"
 }
 module.exports=config;
